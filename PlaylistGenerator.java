0a23a5bab905ba9d67cdf4488c46542a64e4d072
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-09-12 22:33:13 +0200

    Minor refactoring

--- PlaylistGenerator.java
+++ PlaylistGenerator.java
@@ -4,9 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 
 public class PlaylistGenerator {
 
