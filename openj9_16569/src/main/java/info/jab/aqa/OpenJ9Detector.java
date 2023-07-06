7c71772111940b27c7a39865742479e1869a4fd3
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-07-06 18:19:02 +0200

    Improved detector

--- openj9_16569/src/main/java/info/jab/aqa/OpenJ9Detector.java
+++ openj9_16569/src/main/java/info/jab/aqa/OpenJ9Detector.java
@@ -3,8 +3,14 @@ package info.jab.aqa;
 public class OpenJ9Detector {
 
     public boolean detect() {
-        String jvmName = System.getProperty("java.vm.name");
-        return jvmName.contains("OpenJ9");
+        String impl = System.getProperty("java.vm.name");
+        System.out.println("System.getProperty('java.vm.name')=" + impl + "\n");
+        impl = impl.toLowerCase();
+        if (impl.contains("ibm")) {
+            return true;
+        } else if (impl.contains("openj9")) {
+            return true;
+        }
+        return false;
     }
-
 }
